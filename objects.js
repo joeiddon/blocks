a4c239a64cb3aa658763af26a8094152baf47e80
Callum Iddon <callum.iddon@gmail.com>
2018-08-11 11:43:19 +0100

    Format object.js

--- objects.js
+++ objects.js
@@ -20,14 +20,16 @@ let objects = {
                 {verts: [{x:0, y:0, z:1}, {x:1, y:0, z:1}, {x:1, y:1, z:1}, {x:0, y:1, z:1}], col: {h: 90, s: 80, l: 40}, vect: {x: 0, y: 0, z: 1}}];
     },
     person: function(){
+        // in the x,y plane with z upwards
+        // looks down their own y axis
         let col = {h: 131, s: 84, l: 57};
         let sz = 0.5;
         let h  = player_height;
-        return [{verts: [{x:-sz/2, y:-sz/2, z:0}, {x:-sz/2, y:sz/2, z:0}, {x:-sz/2, y:sz/2, z:h}, {x:-sz/2, y:-sz/2, z:h}], col: col, vect: {x:-1, y: 0, z: 0}},
-                {verts: [{x:-sz/2, y:-sz/2, z:0}, {x:sz/2, y:-sz/2, z:0}, {x:sz/2, y:-sz/2, z:h}, {x:-sz/2, y:-sz/2, z:h}], col: col, vect: {x: 0, y:-1, z: 0}},
-                {verts: [{x:-sz/2, y:-sz/2, z:0}, {x:sz/2, y:-sz/2, z:0}, {x:sz/2, y:sz/2, z:0}, {x:-sz/2, y:sz/2, z:0}], col: col, vect: {x: 0, y: 0, z:-2}},
-                {verts: [{x:sz/2, y:-sz/2, z:0}, {x:sz/2, y:sz/2, z:0}, {x:sz/2, y:sz/2, z:h}, {x:sz/2, y:-sz/2, z:h}], col: col, vect: {x: 1, y: 0, z: 0}},
-                {verts: [{x:-sz/2, y:sz/2, z:0}, {x:sz/2, y:sz/2, z:0}, {x:sz/2, y:sz/2, z:h}, {x:-sz/2, y:sz/2, z:h}], col: col, vect: {x: 0, y: 1, z: 0}},
-                {verts: [{x:-sz/2, y:-sz/2, z:h}, {x:sz/2, y:-sz/2, z:h}, {x:sz/2, y:sz/2, z:h}, {x:-sz/2, y:sz/2, z:h}], col: col, vect: {x: 0, y: 0, z: 2}}];
+        return [{verts: [{x:-sz/2, y:-sz/2, z:0}, {x:-sz/2, y: sz/2, z:0}, {x:-sz/2, y: sz/2, z:h}, {x:-sz/2, y:-sz/2, z:h}], col: col, vect: {x:-1, y: 0, z: 0}},
+                {verts: [{x:-sz/2, y:-sz/2, z:0}, {x: sz/2, y:-sz/2, z:0}, {x: sz/2, y:-sz/2, z:h}, {x:-sz/2, y:-sz/2, z:h}], col: col, vect: {x: 0, y:-1, z: 0}},
+                {verts: [{x:-sz/2, y:-sz/2, z:0}, {x: sz/2, y:-sz/2, z:0}, {x: sz/2, y: sz/2, z:0}, {x:-sz/2, y: sz/2, z:0}], col: col, vect: {x: 0, y: 0, z:-1}},
+                {verts: [{x: sz/2, y:-sz/2, z:0}, {x: sz/2, y: sz/2, z:0}, {x: sz/2, y: sz/2, z:h}, {x: sz/2, y:-sz/2, z:h}], col: col, vect: {x: 1, y: 0, z: 0}},
+                {verts: [{x:-sz/2, y: sz/2, z:0}, {x: sz/2, y: sz/2, z:0}, {x: sz/2, y: sz/2, z:h}, {x:-sz/2, y: sz/2, z:h}], col: col, vect: {x: 0, y: 1, z: 0}},
+                {verts: [{x:-sz/2, y:-sz/2, z:h}, {x: sz/2, y:-sz/2, z:h}, {x: sz/2, y: sz/2, z:h}, {x:-sz/2, y: sz/2, z:h}], col: col, vect: {x: 0, y: 0, z: 1}}];
     }
 }
